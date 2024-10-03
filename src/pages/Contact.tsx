9b7b97ff1e3586d3462f7aaf1b55b533eac34707
dthanh <dthanhff318@gmail.com>
2024-10-03 17:19:21 +0700

    fix: change position

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -42,8 +42,8 @@ const Contact = () => {
 		<div className="w-full flex flex-col items-center gap-4">
 			<p className="text-text-color text-center text-2xl">Contact me</p>
 			<div className="">
-				<MemoryCard />
 				<div>{shake ? <p>Device Shaking!</p> : <p>No Shake</p>}</div>
+				<MemoryCard />
 			</div>
 		</div>
 	);
