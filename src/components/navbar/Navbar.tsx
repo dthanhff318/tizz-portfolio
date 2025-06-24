0bc43bd88ee0947a318d54ed5c7c0af745c167bf
dthanh <dthanhff318@gmail.com>
2025-06-24 12:01:11 +0700

    fix: remove redundant import

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-import useNavbarServices from "./navbar.service";
-
 const Navbar = () => {
 	return (
 		<nav className="w-[50px] lg:w-[80px] h-full bottom-7 left-2 items-center flex flex-col gap-2 bg-primary-bold p-2 rounded-3xl  transition-all"></nav>
