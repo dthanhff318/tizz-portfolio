44491afada990b4fa9711d51d38e56987c316078
dthanh <dthanhff318@gmail.com>
2025-06-24 12:00:24 +0700

    fix: fix build

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,8 @@
 import useNavbarServices from "./navbar.service";
 
 const Navbar = () => {
-	const { navList } = useNavbarServices();
 	return (
-		<nav className="w-[50px] lg:w-[80px] h-full bottom-7 left-2 items-center flex flex-col gap-2 bg-primary-bold p-2 rounded-3xl  transition-all">
-			{navList.map((e, i) => (
-				<div></div>
-			))}
-		</nav>
+		<nav className="w-[50px] lg:w-[80px] h-full bottom-7 left-2 items-center flex flex-col gap-2 bg-primary-bold p-2 rounded-3xl  transition-all"></nav>
 	);
 };
 
