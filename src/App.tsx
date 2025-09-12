35f1de6f3c2d368359e4aa075727b4413429ba52
dthanh <dthanhff318@gmail.com>
2025-09-12 17:31:32 +0700

    feat: intergrate GA

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import type { FunctionComponent } from "./common/types";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useState } from "react";
 import MainLoading from "./components/loading/MainLoading.tsx";
+import ReactGA from "react-ga4";
+
+// Analyze using Google Analytics
+ReactGA.initialize("G-6SHWN9NK16");
 
 type AppProps = { router: ReturnType<typeof createBrowserRouter> };
 
